056d292e74306371f5c276a03e9e11c9d14822e1
Mateus Koppe <mateus.koppe@gmail.com>
2018-04-01 17:46:59 -0300

    Add mine endpoint

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const Blockchain = require('../blockchain')
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001
@@ -6,8 +7,15 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001
 const app = express()
 const blockchain = new Blockchain()
 
+app.use(bodyParser.json())
+
 app.get('/blocks', (req, res) => {
   res.json(blockchain.chain)
+})
+
+app.post('/mine', (req, res) => {
+  blockchain.addBlock(req.body.data)
+  res.redirect('/blocks')
 })
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`))
