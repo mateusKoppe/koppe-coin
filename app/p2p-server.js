6aef14ddd3dc29e9e8118ac4e311a449c92e74a3
Mateus Koppe <mateus.koppe@gmail.com>
2018-05-13 15:59:48 -0300

    Send blockchain for new connections

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -25,7 +25,7 @@ class P2pServer {
 
   connectSocket (socket) {
     this.sockets.push(socket)
-    socket.send(JSON.stringify(this.blockchain.chain))
+    this.sendChain(socket)
 
     this.messageHandler(socket)
   }
@@ -33,8 +33,12 @@ class P2pServer {
   messageHandler (socket) {
     socket.on('message', message => {
       const data = JSON.parse(message)
-      console.log('data', data)
+      this.blockchain.replaceChain(data)
     })
+  }
+
+  sendChain (socket) {
+    socket.send(JSON.stringify(this.blockchain.chain))
   }
 }
 
