4f3aa0881c44b603cd68093fae0eb08511193df3
Mateus Koppe <mateus.koppe@gmail.com>
2018-05-13 23:20:37 -0300

    Implement a MINE_RATE algoritmn

--- blockchain/block.js
+++ blockchain/block.js
@@ -1,50 +1,61 @@
 const SHA256 = require('crypto-js/sha256')
-const { DIFFICULTY } = require('../config')
+const { DIFFICULTY, MINE_RATE } = require('../config')
 
 class Block {
-  constructor (timestamp, lastHash, hash, data, nonce) {
+  constructor (timestamp, lastHash, hash, data, nonce, difficulty) {
     this.timestamp = timestamp
     this.lastHash = lastHash
     this.hash = hash
     this.data = data
     this.nonce = nonce
+    this.difficulty = difficulty || DIFFICULTY
   }
 
   toString () {
     return `Block -
-      Timestamp: ${this.timestamp}
-      Last Hash: ${this.lastHash.substring(0, 10)}...
-      Hash     : ${this.hash.substring(0, 10)}...
-      Nonce    : ${this.nonce}
-      Data     : ${this.data}
+      Timestamp : ${this.timestamp}
+      Last Hash : ${this.lastHash.substring(0, 10)}...
+      Hash      : ${this.hash.substring(0, 10)}...
+      Nonce     : ${this.nonce}
+      Difficulty: ${this.difficulty}
+      Data      : ${this.data}
     `
   }
 
   static genesis () {
-    return new this(0, '', 'first-hash', [], 0)
+    return new this(0, '', 'first-hash', [], 0, DIFFICULTY)
   }
 
-  static generateHash (timestamp, lastHash, data, nonce) {
-    return SHA256(`${timestamp}${lastHash}${data}${nonce}`).toString()
+  static generateHash (timestamp, lastHash, data, nonce, difficulty) {
+    return SHA256(
+      `${timestamp}${lastHash}${data}${nonce}${difficulty}`
+    ).toString()
   }
 
   static generateBlockHash (block) {
-    const {timestamp, lastHash, data, nonce} = block
-    return Block.generateHash(timestamp, lastHash, data, nonce)
+    const {timestamp, lastHash, data, nonce, difficulty} = block
+    return Block.generateHash(timestamp, lastHash, data, nonce, difficulty)
   }
 
   static mineBlock (lastBlock, data) {
     const lastHash = lastBlock.hash
     let nonce = 0
-    let hash, timestamp
+    let hash, timestamp, difficulty
 
     do {
       timestamp = Date.now()
       nonce++
-      hash = Block.generateHash(timestamp, lastHash, data, nonce)
-    } while (hash.substring(0, DIFFICULTY) !== '0'.repeat(DIFFICULTY))
+      difficulty = Block.adjustDifficulty(lastBlock, timestamp)
+      hash = Block.generateHash(timestamp, lastHash, data, nonce, difficulty)
+    } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty))
 
-    return new this(timestamp, lastHash, hash, data, nonce)
+    return new this(timestamp, lastHash, hash, data, nonce, difficulty)
+  }
+
+  static adjustDifficulty (lastBlock, currenctTime) {
+    const { difficulty } = lastBlock
+    const isTooHard = lastBlock.timestamp + MINE_RATE > currenctTime
+    return isTooHard ? difficulty + 1 : difficulty - 1
   }
 }
 
--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports.DIFFICULTY = 4
+module.exports.MINE_RATE = 15000
 module.exports.DEFAULT_HTTP_PORT = 3001
 module.exports.DEFAULT_P2P_PORT = 5001
