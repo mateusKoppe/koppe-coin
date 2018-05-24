afdb24e29280094ddbb4337fdc30569a00251f8b
Mateus Koppe <mateus.koppe@gmail.com>
2018-05-24 13:28:13 -0300

    Implement ChainUtil class

---
+++ chain-util.js
@@ -0,0 +1,15 @@
+const EC = require('elliptic').ec
+const uuidV1 = require('uuid/v1')
+const ec = new EC('secp256k1')
+
+class ChainUtil {
+  static genKeyPair () {
+    return ec.genKeyPair()
+  }
+
+  static id () {
+    return uuidV1()
+  }
+}
+
+module.exports = ChainUtil
