eaee624b8d091fc6bf3317d98ce1ef6eabe7674a
Mateus Koppe <mateus.koppe@gmail.com>
2018-05-31 23:29:25 -0300

    Create end-points to list and add transactions

--- app/index.js
+++ app/index.js
@@ -2,12 +2,16 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const Blockchain = require('../blockchain')
 const P2pServer = require('./p2p-server')
+const Wallet = require('../wallet')
+const TransactionPool = require('../wallet/transaction-pool')
 const { DEFAULT_HTTP_PORT } = require('../config')
 
 const HTTP_PORT = process.env.HTTP_PORT || DEFAULT_HTTP_PORT
 
 const app = express()
 const blockchain = new Blockchain()
+const wallet = new Wallet()
+const tp = new TransactionPool()
 const p2pServer = new P2pServer(blockchain)
 
 app.use(bodyParser.json())
@@ -20,6 +24,16 @@ app.post('/mine', (req, res) => {
   blockchain.addBlock(req.body.data)
   p2pServer.syncChains()
   res.redirect('/blocks')
+})
+
+app.get('/transactions', (req, res) => {
+  res.json(tp.transactions)
+})
+
+app.post('/transactions', (req, res) => {
+  const { recipient, amount } = req.body
+  wallet.createTransaction(recipient, amount, tp)
+  res.redirect('/transactions')
 })
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`))
