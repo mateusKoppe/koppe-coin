b60689db81690589af46c4ff381016c81b9e8028
Mateus Koppe <mateus.koppe@gmail.com>
2018-05-31 22:04:34 -0300

    Implement TrascttionPoll

---
+++ wallet/transaction-pool.test.js
@@ -0,0 +1,29 @@
+const TransactionPool = require('./transaction-pool')
+const Transaction = require('./transaction')
+const Wallet = require('../wallet')
+
+describe('TransactionPool', () => {
+  let tp, wallet, transaction
+
+  beforeEach(() => {
+    tp = new TransactionPool()
+    wallet = new Wallet()
+    transaction = Transaction.newTrasaction(wallet, 'r4nd-4dr355', 30)
+    tp.updateOrAddTransaction(transaction)
+  })
+
+  it('adds a transaction to the pool', () => {
+    expect(tp.transactions.find(t => t.id === transaction.id))
+      .toEqual(transaction)
+  })
+
+  it('updates a transaction in the poll', () => {
+    const oldTransaction = JSON.stringify(transaction)
+    const newTrasaction = transaction.update(wallet, 'foo-4ddr355', 40)
+    tp.updateOrAddTransaction(newTrasaction)
+    const transactionInPool = tp.transactions.find(t => t.id === newTrasaction.id)
+
+    expect(JSON.stringify(transactionInPool))
+      .not.toEqual(oldTransaction)
+  })
+})
