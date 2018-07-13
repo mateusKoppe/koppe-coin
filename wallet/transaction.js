16c378c0e505638171157d8f6879a377699cfcc9
Mateus Koppe <mateus.koppe@gmail.com>
2018-07-12 23:01:51 -0300

    Implement rewardTransaction method

--- wallet/transaction.js
+++ wallet/transaction.js
@@ -1,4 +1,5 @@
 const ChainUtil = require('../chain-util')
+const { MINING_REWARD } = require('../config')
 
 class Transaction {
   constructor () {
@@ -23,20 +24,31 @@ class Transaction {
     return this
   }
 
-  static newTransaction (senderWallet, recipient, amount) {
+  static transactionWithOuputs (senderWallet, outputs) {
     const transaction = new this()
+    transaction.outputs.push(outputs)
+    Transaction.signTransaction(transaction, senderWallet)
+    return transaction
+  }
 
+  static newTransaction (senderWallet, recipient, amount) {
     if (amount > senderWallet.balance) {
       return
     }
 
-    transaction.outputs.push(...[
+    const transaction = this.transactionWithOuputs(senderWallet, [
       { amount: senderWallet.balance - amount, address: senderWallet.publicKey },
       { amount, address: recipient }
     ])
-    Transaction.signTransaction(transaction, senderWallet)
 
     return transaction
+  }
+
+  static rewardTransaction (minerWallet, blockchainWallet) {
+    return Transaction.transactionWithOuputs(blockchainWallet, [{
+      amount: MINING_REWARD,
+      address: minerWallet.publicKey
+    }])
   }
 
   static signTransaction (transaction, senderWallet) {
